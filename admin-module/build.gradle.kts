c98300755336251d5676794795f7ed796e4de357
hyune-c <choi8608@gmail.com>
2022-10-30 01:40:22 +0900

    :wrench: [admin-module] set dependency - webflux swagger

--- admin-module/build.gradle.kts
+++ admin-module/build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
 }
 
 dependencies {
-
+    // webflux swagger
+    implementation("org.springdoc:springdoc-openapi-webflux-core:1.6.12")
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.12")
 }
