ee795d1712899e3b742fa99b37bb1670d2ce86fb
hyune-c <choi8608@gmail.com>
2022-10-30 04:23:15 +0900

    :wrench: [admin-module] set dependency - jpa

--- admin-module/build.gradle.kts
+++ admin-module/build.gradle.kts
@@ -6,4 +6,9 @@ dependencies {
     // webflux swagger
     implementation("org.springdoc:springdoc-openapi-webflux-core:1.6.12")
     implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.12")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("mysql:mysql-connector-java")
 }
