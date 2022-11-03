95f1ba3f860c8520dc9861a94962fa6101b44b38
hyune-c <choi8608@gmail.com>
2022-11-04 02:39:03 +0900

    :wrench: [admin-module] set dependency - testcontainers

--- admin-module/build.gradle.kts
+++ admin-module/build.gradle.kts
@@ -11,4 +11,9 @@ dependencies {
 
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
+
+    // testcontainers
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.4")
+    testImplementation("org.testcontainers:mysql:1.17.4")
 }
