c2f042d0cc2912c1dad7a2ca43dc141dacf6797f
Hyune-c <choi8608@gmail.com>
2022-12-17 04:01:13 +0900

    ✅ [admin-module] 테스트 코드 개선

--- admin-module/src/test/kotlin/com/hyunec/domain/merchant/service/MerchantRegisterServiceTest.kt
+++ admin-module/src/test/kotlin/com/hyunec/domain/merchant/service/MerchantRegisterServiceTest.kt
@@ -4,27 +4,20 @@ import com.hyunec.domain.merchant.repository.MerchantRepository
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
 @DisplayName("MerchantRegisterServiceTest")
 @SpringBootTest
-internal class MerchantRegisterServiceTest : BehaviorSpec() {
-    override fun extensions() = listOf(SpringExtension)
-
-    @Autowired
-    private lateinit var merchantRegisterService: MerchantRegisterService
-
-    @Autowired
-    private lateinit var merchantRepository: MerchantRepository
-
+internal class MerchantRegisterServiceTest(
+    val merchantRegisterService: MerchantRegisterService,
+    val merchantRepository: MerchantRepository,
+) : BehaviorSpec() {
     init {
         this.Given("상점 정보 준비")
         {
